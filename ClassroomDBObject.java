35c8d023e711008c343eda2c65653b8aab5c9d6f
NIR BEN SHALOM <nir.benshalom@live.biu.ac.il>
2018-02-23 18:46:11 +0200

    Parsed and created classroom object

--- ClassroomDBObject.java
+++ ClassroomDBObject.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class ClassroomDBObject {
 
-    private int class_number;
-    private int building_number;
+    private int classNumber;
+    private int buildingNumber;
     private ArrayList<ArrayList<Integer>> times;
+    private String day;
+    private char semester;
 
 
     public ClassroomDBObject() {
@@ -13,14 +15,35 @@ public class ClassroomDBObject {
     }
 
     public void SetClassNumber(int num) {
-        class_number = num;
+        classNumber = num;
+    }
+    public int getClassNumber () {
+        return classNumber;
     }
 
     public void SetBuildingNumber(int num) {
-        building_number = num;
+        buildingNumber = num;
+    }
+    public int getBuildingNumber() {
+        return buildingNumber;
     }
 
     public void AddTime(ArrayList<Integer> time) {
         times.add(time);
+    }
+
+    public void setSemester(char sem) {
+        semester = sem;
+    }
+    public char getSemester() {
+        return semester;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
     }
 }
